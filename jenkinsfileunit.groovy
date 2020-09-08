c20e908cdd5d804e782c5f629c4558791eef31a2
utkarsh upadhyay <utkarshu91@gmail.com>
2020-09-08 19:48:45 +0530

    Update jenkinsfileunit.groovy

--- jenkinsfileunit.groovy
+++ jenkinsfileunit.groovy
@@ -13,7 +13,7 @@ node('docker-node') {
                 echo "scmVars: ${scmVars}"
                 filebranchname="${scmVars.GIT_BRANCH}"
                 filegitcommit="${scmVars.GIT_COMMIT}"
-                bash envile.sh
+                sh "bash envile.sh"
                 echo "scmVars.GIT_COMMIT: ${scmVars.GIT_COMMIT}"
                 echo "scmVars.GIT_BRANCH: ${scmVars.GIT_BRANCH}"
                 sh """
